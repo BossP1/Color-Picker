95a08ea2f8c667a7f7da6e13af4e70e0a0ee1c26
Bernie Pineda <44620231+BossP1@users.noreply.github.com>
2020-12-30 22:24:03 -0600

    Primera subida de todos los archivos

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+document.getElementById('color').addEventListener('input', getColor);
+
+function getColor(){
+    let color = document.getElementById('color').value;
+
+    document.getElementById('visualizar').style.background = color;
+    document.getElementById('visualizar').innerHTML = color;
+}
